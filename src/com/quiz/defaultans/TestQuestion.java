5df72eddb5d8d224e989e96042a192546c7ba012
Mathuragini <mathusharma15@gmail.com>
2019-08-10 15:46:38 +0530

    updated

--- src/com/quiz/defaultans/TestQuestion.java
+++ src/com/quiz/defaultans/TestQuestion.java
@@ -11,19 +11,23 @@ public class TestQuestion {
 		
 		//Create object for the Question Class and add question and questionNo
 		Question question = new Question(1," What is the color of Mango?");
+		Question question1 = new Question(1," What is the color of sky?");
 		
 		//Set the choices for the above question
 		question.setChoices("Red","Green","Yellow","Black","Blue");
+		question1.setChoices("Red","Green","Yellow","Black","Blue");
 		
 		//Set the correct answer for the question
 		question.setCorrectAnswer(3);
+		question1.setCorrectAnswer(5);
 		
 		//Add the question to the questionList
 		questionList.add(question);
+		questionList.add(question1);
 		
 		//Print the question and answers
 		for(Question ques :questionList ) {
-			System.out.print(ques);
+			System.out.println(ques);
 		}
 
 	}
--- src/com/quiz/userinput/TestQuestion.java
+++ src/com/quiz/userinput/TestQuestion.java
@@ -1,3 +1,4 @@
+
 package com.quiz.userinput;
 
 
